48e87eb631eb1cbc9517ea511238ed2fcaa913f0
KarthikAkaike <karthik@akaiketech.com>
2025-02-03 09:57:06 +0530

    small fixes

--- app/routers/tracking.py
+++ app/routers/tracking.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Request, Response
+from fastapi import APIRouter, HTTPException, Request, Response
+from loguru import logger
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
+from app.config import settings
 from app.database import SessionLocal
 from app.models import TrackingEvent
 from app.utils import get_image_bytes
@@ -12,30 +14,44 @@ router = APIRouter()
 @router.get("/track/{email_id}")
 async def track_email(email_id: str, request: Request):
     db: Session = SessionLocal()
-    tracking_event = TrackingEvent(
-        email_id=email_id,
-        user_agent=request.headers.get("User-Agent"),
-        ip_address=request.client.host,
-    )
-    db.add(tracking_event)
-    db.commit()
-    db.refresh(tracking_event)
-    db.close()
+    try:
+        tracking_event = TrackingEvent(
+            email_id=email_id,
+            user_agent=request.headers.get("User-Agent"),
+            ip_address=request.client.host,
+        )
+        db.add(tracking_event)
+        db.commit()
+        db.refresh(tracking_event)
 
-    # Return a 1x1 transparent pixel
-    icon = get_image_bytes("../assets/icon.png")
-    return Response(content=icon, media_type="image/gif")
+        # Return a 1x1 transparent pixel
+        icon = get_image_bytes("../assets/icon.png")
+        return Response(content=icon, media_type="image/gif")
+    except Exception as e:
+        logger.error(f"Error tracking email: {e}")
+        logger.exception(e)
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    finally:
+        db.close()
 
 
 @router.get("/track_counts")
 async def get_track_counts():
     db: Session = SessionLocal()
-    track_counts = (
-        db.query(
-            TrackingEvent.email_id, func.count(TrackingEvent.email_id).label("count")
+    try:
+        track_counts = (
+            db.query(
+                TrackingEvent.email_id,
+                func.count(TrackingEvent.email_id).label("count"),
+            )
+            .group_by(TrackingEvent.email_id)
+            .all()
         )
-        .group_by(TrackingEvent.email_id)
-        .all()
-    )
-    db.close()
-    return track_counts
+        return track_counts
+    except Exception as e:
+        logger.error(f"Error getting track counts: {e}")
+        logger.exception(e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    finally:
+        db.close()
